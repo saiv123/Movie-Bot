6e2b553b76e86ef1678a1a13a98bb1979b1980cf
saiv123 <saiveeravelli2000@gmail.com>
2025-04-08 12:07:47 -0800

    Refactor Check function to return media type and enhance add_with_id command for adding items to watchlist

--- bot.py
+++ bot.py
@@ -7,9 +7,11 @@ from secret import token, apikey
 import requests
 
 #? might want to have check return the id as well instead of searching the json
+#TODO: create function for adding media with id and type
 
 bot = interactions.Client()
 
+#replace the current url api with this one url = "https://api.themoviedb.org/3/search/multi?query=namehere&language=en-US&page=1"
 def Check(name: str) -> tuple[bool, dict | None, str | None]:
     """
     A Function that checks the name of a movie or tv show
@@ -23,12 +25,11 @@ def Check(name: str) -> tuple[bool, dict | None, str | None]:
         "accept": "application/json",
         "Authorization": apikey
         }
-    types = ["movie", "tv"]
-    for Type in types:
-        url = f"https://api.themoviedb.org/3/search/{Type}?query={name}&language=en-US&page=1"
-        response = requests.get(url, headers=header).json()
-        if response["total_results"] != 0 :
-            return (response["total_results"] != 0, response, Type)
+    url = f"https://api.themoviedb.org/3/search/multi?query={name}&language=en-US&page=1"
+    response = requests.get(url, headers=header).json()
+    if response["total_results"] != 0:
+        media_type = response["results"][0]["media_type"]
+        return (True, response, media_type)
     return (False, None, None)
 
 def addToWatchList(id:int, typeS:str)->bool:
@@ -56,11 +57,6 @@ def addToWatchList(id:int, typeS:str)->bool:
     response = requests.post(url, json=data, headers=header).json()
     return response["success"]
 
-def ButtonCheck(component: Component)->bool:
-    if component.ctx.custom_id == "yes":
-        return True
-    return False
-
 @listen()
 async def on_startup():
     print("Bot ready")
@@ -85,41 +81,59 @@ async def addToListContext(ctx: ContextMenuContext):
 async def add_to_watchlist(ctx: SlashContext, name: str):
     (status, json, ty) = Check(name)
     if status:
-        if json["total_results"]:
-            components = ActionRow(
+        components = ActionRow(
                 Button(custom_id="yes", style=ButtonStyle.GREEN, label="Yes"),
                 Button(custom_id="no", style=ButtonStyle.RED, label="No")
             )
-
-            await ctx.send(json["results"][0]["original_name"], components=components)
-            try:
-                used_component: Component = await bot.wait_for_component(components=components, check=ButtonCheck(), timeout=30)
-            except TimeoutError:
-                await ctx.send("Timed out, run the command again to add the show or movie to the watchlist")
+        await ctx.send(json["results"][0]["original_name"], components=components)
+        try:
+            used_component: Component = await bot.wait_for_component(components=components, timeout=30)
+        except TimeoutError:
+            await ctx.send("Timed out, run the command again to add the show or movie to the watchlist")
+            return
+        
         if used_component.ctx.custom_id == "yes":
             addToWatchList(json["results"][0]["id"], ty)
-            await ctx.send(f"{name} has been added to the watchlist")
+            await ctx.send(f"{json['results'][0]['original_name']} has been added to the watchlist")
         else:
-            await ctx.send(f"{name} was not added to the watchlist")
+            embed = interactions.Embed(title=f"List of the {'TV show' if ty == 'tv' else 'movie'}",description="List of the movies or shows that were found use add with id command to add them to the watchlist", images=json)
+            for i in json["results"]:
+                embed.add_field(name="Title", value=f"{i['original_name']} - {str(i['id'])}")
+            await ctx.send(embeds=embed)
     else:
         await ctx.send(f"{name} was not found to be a movie or TV show")
-##Command deprecated, will only allow the right click and add or with sending a message in a channel specified
-# @slash_command(name="scan", description="will scan the channel for movies and shows")
-# async def scan(ctx: SlashContext):
-#     messages:list[str] = []
-#     if ctx.channel.message_count >= 50:
-#         await ctx.send("Sorry there is too many messages in this channel, you will need to add the movies or shows manually.")
-#     else:
-#         history = ctx.channel.history(limit=0)
-#         messages = history.flatten()
+
+@slash_command(name="add_with_id", description="Add a movie or TV show to the watchlist with id")
+@slash_option(
+    name="id",
+    description="ID of the movie or TV show",
+    required=True,
+    opt_type=OptionType.INTEGER
+)
+async def add_with_id(ctx: SlashContext, id: int):
+    headers = {
+        "accept": "application/json",
+        "Authorization": apikey
+    }
     
-#     if len(messages) > 0:
-#         for message in messages:
-#             temp:str = message.split("\n")
-#             for e in temp:
-#                 (status, json) = Is_A_Movie(e)
-#                 if status:
-#                     addToWatchList(json["results"][0]["id"], "movie")
-#                 else:
-#                     await ctx.send(f"{e} was not found to be a movie")
+    # Check if the ID corresponds to a TV show
+    tv_url = f"https://api.themoviedb.org/3/tv/{id}?language=en-US"
+    tv_response = requests.get(tv_url, headers=headers).json()
+    
+    if "id" in tv_response:
+        media_type = "tv"
+        if addToWatchList(id, media_type):
+            await ctx.send(f"{tv_response['name']} has been added to the watchlist")
+    
+    # Check if the ID corresponds to a movie
+    movie_url = f"https://api.themoviedb.org/3/movie/{id}?language=en-US"
+    movie_response = requests.get(movie_url, headers=headers).json()
+    
+    if "id" in movie_response:
+        media_type = "movie"
+        if addToWatchList(id, media_type):
+            await ctx.send(f"{movie_response['title']} has been added to the watchlist")
+    else:
+        await ctx.send(f"Movie or TV show with ID {id} not found")
+
 bot.start(token)
