a1fad222ec71b55d7b4731abf86e7a6f16e7ad62
saiv123 <saiveeravelli2000@gmail.com>
2025-04-08 13:13:26 -0800

    trying to fix async functions running during tests

--- bot.py
+++ bot.py
@@ -56,83 +56,84 @@ def addToWatchList(id:int, typeS:str)->bool:
     response = requests.post(url, json=data, headers=header).json()
     return response["success"]
 
-@listen()
-async def on_startup():
-    print("Bot ready")
 
-@message_context_menu(name="Add movie to list")
-async def addToListContext(ctx: ContextMenuContext):
-    message: Message = ctx.target
-    (status, json, ty) = Check(message.content)
-    if status:
-        addToWatchList(json["results"][0]["id"], ty)
-        await ctx.send(f"{message.content} has been added to the list")
-    else:
-        await ctx.send(f"{message.content} was not found to be a movie\nIf there is multiple names in one message I can't process that yet ;(")
+if __name__ == "__main__":
+    @listen()
+    async def on_startup():
+        print("Bot ready")
 
-@slash_command(name="add_to_watchlist", description="Add a movie or TV show to the watchlist")
-@slash_option(
-    name="name",
-    description="Name of the movie or TV show",
-    required=True,
-    opt_type=OptionType.STRING
-)
-async def add_to_watchlist(ctx: SlashContext, name: str):
-    (status, json, ty) = Check(name)
-    if status:
-        components = ActionRow(
-                Button(custom_id="yes", style=ButtonStyle.GREEN, label="Yes"),
-                Button(custom_id="no", style=ButtonStyle.RED, label="No")
-            )
-        await ctx.send(json["results"][0]["original_name"], components=components)
-        try:
-            used_component: Component = await bot.wait_for_component(components=components, timeout=30)
-        except TimeoutError:
-            await ctx.send("Timed out, run the command again to add the show or movie to the watchlist")
-            return
-        
-        if used_component.ctx.custom_id == "yes":
+    @message_context_menu(name="Add movie to list")
+    async def addToListContext(ctx: ContextMenuContext):
+        message: Message = ctx.target
+        (status, json, ty) = Check(message.content)
+        if status:
             addToWatchList(json["results"][0]["id"], ty)
-            await ctx.send(f"{json['results'][0]['original_name']} has been added to the watchlist")
+            await ctx.send(f"{message.content} has been added to the list")
         else:
-            embed = interactions.Embed(title=f"List of the {'TV show' if ty == 'tv' else 'movie'}",description="List of the movies or shows that were found use add with id command to add them to the watchlist", images=json)
-            for i in json["results"]:
-                embed.add_field(name="Title", value=f"{i['original_name']} - {str(i['id'])}")
-            await ctx.send(embeds=embed)
-    else:
-        await ctx.send(f"{name} was not found to be a movie or TV show")
+            await ctx.send(f"{message.content} was not found to be a movie\nIf there is multiple names in one message I can't process that yet ;(")
 
-@slash_command(name="add_with_id", description="Add a movie or TV show to the watchlist with id")
-@slash_option(
-    name="id",
-    description="ID of the movie or TV show",
-    required=True,
-    opt_type=OptionType.INTEGER
-)
-async def add_with_id(ctx: SlashContext, id: int):
-    headers = {
-        "accept": "application/json",
-        "Authorization": apikey
-    }
-    
-    # Check if the ID corresponds to a TV show
-    tv_url = f"https://api.themoviedb.org/3/tv/{id}?language=en-US"
-    tv_response: dict = requests.get(tv_url, headers=headers).json()
-    
-    if "id" in tv_response:
-        media_type = "tv"
-        if addToWatchList(id, media_type):
-            await ctx.send(f"{tv_response['name']} has been added to the watchlist")
-    
-    # Check if the ID corresponds to a movie
-    movie_url = f"https://api.themoviedb.org/3/movie/{id}?language=en-US"
-    movie_response = requests.get(movie_url, headers=headers).json()
-    
-    if "id" in movie_response:
-        media_type = "movie"
-        if addToWatchList(id, media_type):
-            await ctx.send(f"{movie_response['title']} has been added to the watchlist")
-    else:
-        await ctx.send(f"Movie or TV show with ID {id} not found")
+    @slash_command(name="add_to_watchlist", description="Add a movie or TV show to the watchlist")
+    @slash_option(
+        name="name",
+        description="Name of the movie or TV show",
+        required=True,
+        opt_type=OptionType.STRING
+    )
+    async def add_to_watchlist(ctx: SlashContext, name: str):
+        (status, json, ty) = Check(name)
+        if status:
+            components = ActionRow(
+                    Button(custom_id="yes", style=ButtonStyle.GREEN, label="Yes"),
+                    Button(custom_id="no", style=ButtonStyle.RED, label="No")
+                )
+            await ctx.send(json["results"][0]["original_name"], components=components)
+            try:
+                used_component: Component = await bot.wait_for_component(components=components, timeout=30)
+            except TimeoutError:
+                await ctx.send("Timed out, run the command again to add the show or movie to the watchlist")
+                return
+            
+            if used_component.ctx.custom_id == "yes":
+                addToWatchList(json["results"][0]["id"], ty)
+                await ctx.send(f"{json['results'][0]['original_name']} has been added to the watchlist")
+            else:
+                embed = interactions.Embed(title=f"List of the {'TV show' if ty == 'tv' else 'movie'}",description="List of the movies or shows that were found use add with id command to add them to the watchlist", images=json)
+                for i in json["results"]:
+                    embed.add_field(name="Title", value=f"{i['original_name']} - {str(i['id'])}")
+                await ctx.send(embeds=embed)
+        else:
+            await ctx.send(f"{name} was not found to be a movie or TV show")
 
-bot.start(token)
+    @slash_command(name="add_with_id", description="Add a movie or TV show to the watchlist with id")
+    @slash_option(
+        name="id",
+        description="ID of the movie or TV show",
+        required=True,
+        opt_type=OptionType.INTEGER
+    )
+    async def add_with_id(ctx: SlashContext, id: int):
+        headers = {
+            "accept": "application/json",
+            "Authorization": apikey
+        }
+        
+        # Check if the ID corresponds to a TV show
+        tv_url = f"https://api.themoviedb.org/3/tv/{id}?language=en-US"
+        tv_response: dict = requests.get(tv_url, headers=headers).json()
+        
+        if "id" in tv_response:
+            media_type = "tv"
+            if addToWatchList(id, media_type):
+                await ctx.send(f"{tv_response['name']} has been added to the watchlist")
+        
+        # Check if the ID corresponds to a movie
+        movie_url = f"https://api.themoviedb.org/3/movie/{id}?language=en-US"
+        movie_response = requests.get(movie_url, headers=headers).json()
+        
+        if "id" in movie_response:
+            media_type = "movie"
+            if addToWatchList(id, media_type):
+                await ctx.send(f"{movie_response['title']} has been added to the watchlist")
+        else:
+            await ctx.send(f"Movie or TV show with ID {id} not found")
+    bot.start(token)
