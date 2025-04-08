82d32a591c58a62062f83fed059a17e398db0478
saiv123 <saiveeravelli2000@gmail.com>
2025-04-07 16:06:35 -0800

    Add button interaction for watchlist command and enhance add_to_watchlist functionality

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import interactions
-from interactions import slash_command, SlashContext, CommandContext, Option, OptionType, listen
+from interactions import slash_command, SlashContext, slash_option, OptionType, listen
 from interactions import ContextMenuContext, Message, message_context_menu
+from interactions import Button, ButtonStyle, ActionRow
+from interactions.api.events import Component
 from secret import token, apikey
 import requests
 
@@ -53,6 +55,13 @@ def addToWatchList(id:int, typeS:str)->bool:
     }
     response = requests.post(url, json=data, headers=header).json()
     return response["success"]
+
+def ButtonCheck(component: Component)->bool:
+    if component.ctx.custom_id == "yes":
+        return True
+    return False
+
+
 @listen()
 async def on_startup():
     print("Bot ready")
@@ -66,7 +75,35 @@ async def addToListContext(ctx: ContextMenuContext):
         await ctx.send(f"{message.content} has been added to the list")
     else:
         await ctx.send(f"{message.content} was not found to be a movie\nIf there is multiple names in one message I can't process that yet ;(")
-#TODO: Add "add_to_watchlist" command
+
+@slash_command(name="add_to_watchlist", description="Add a movie or TV show to the watchlist")
+@slash_option(
+    name="name",
+    description="Name of the movie or TV show",
+    required=True,
+    opt_type=OptionType.STRING
+)
+async def add_to_watchlist(ctx: SlashContext, name: str):
+    (status, json, ty) = Check(name)
+    if status:
+        if json["total_results"]:
+            components = ActionRow(
+                Button(custom_id="yes", style=ButtonStyle.GREEN, label="Yes"),
+                Button(custom_id="no", style=ButtonStyle.RED, label="No")
+            )
+
+            await ctx.send(json["results"][0]["original_name"], components=components)
+            try:
+                used_component: Component = await bot.wait_for_component(components=components, check=ButtonCheck(), timeout=30)
+            except TimeoutError:
+                await ctx.send("Timed out, run the command again to add the show or movie to the watchlist")
+        if used_component.ctx.custom_id == "yes":
+            addToWatchList(json["results"][0]["id"], ty)
+            await ctx.send(f"{name} has been added to the watchlist")
+        else:
+            await ctx.send(f"{name} was not added to the watchlist")
+    else:
+        await ctx.send(f"{name} was not found to be a movie or TV show")
 ##Command deprecated, will only allow the right click and add or with sending a message in a channel specified
 # @slash_command(name="scan", description="will scan the channel for movies and shows")
 # async def scan(ctx: SlashContext):
